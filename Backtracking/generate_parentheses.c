acf4f44f2ca1ddec6bec16beabccb8dc04d173d5
Keisuke T <kei5328@alumni.stanford.edu>
2022-02-21 21:40:37 -0800

    add solved files.

--- Backtracking/generate_parentheses.c
+++ Backtracking/generate_parentheses.c
@@ -1,6 +1,32 @@
+struct arr_node{
+    char * res;
+    struct arr_node * next;
+}
+
+
+ write_parenthesis()
+ {
+     if (idx>n*2)
+        return;
+     if (n<cnt)
+     {
+        write_parenthesis(idx, cnt-1, n, -1);
+     }
+
+     write_parenthesis(idx, cnt+1, n, 1);
+ }
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 char ** generateParenthesis(int n, int* returnSize){
 
+    for (int ii = 0; ii < n; ii++)
+    {//
+
+        if (is_valid)
+        {
+
+        }
+    }
 }
